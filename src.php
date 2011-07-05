535f4ed505a7ef7c0ecd05f5e7e67dfc9fe44fe1
Hari K T <kthari85@gmail.com>
2011-07-05 20:51:12 +0530

    src.php instance.php and scripts. Need to make some changes and test

---
+++ src.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/src/ConnectionFactory.php';
+require_once __DIR__ . '/src/ConnectionManager.php';
+require_once __DIR__ . '/src/Exception.php';
+require_once __DIR__ . '/src/Select.php';
+require_once __DIR__ . '/src/Connection/AbstractConnection.php';
+require_once __DIR__ . '/src/Connection/Mysql.php';
+require_once __DIR__ . '/src/Connection/Sqlsrv.php';
+require_once __DIR__ . '/src/Connection/SqlsrvDenali.php';
+require_once __DIR__ . '/src/Exception/ConnectionFactory.php';
