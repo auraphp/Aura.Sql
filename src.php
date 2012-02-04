e52a9f8b6c8c62079535dde2136b8169a57162df
Paul M. Jones <pmjones88@gmail.com>
2012-02-03 18:23:10 -0600

    update src and scripts

--- src.php
+++ src.php
@@ -1,8 +1,8 @@
 <?php
-require_once __DIR__ . '/src/Aura/Sql/ProfilerInterface.php';
-require_once __DIR__ . '/src/Aura/Sql/Profiler.php';
 require_once __DIR__ . '/src/Aura/Sql/Driver/AbstractDriver.php';
 require_once __DIR__ . '/src/Aura/Sql/Driver/Mysql.php';
 require_once __DIR__ . '/src/Aura/Sql/Driver/Pgsql.php';
 require_once __DIR__ . '/src/Aura/Sql/Driver/Sqlite.php';
 require_once __DIR__ . '/src/Aura/Sql/DriverFactory.php';
+require_once __DIR__ . '/src/Aura/Sql/ProfilerInterface.php';
+require_once __DIR__ . '/src/Aura/Sql/Profiler.php';
