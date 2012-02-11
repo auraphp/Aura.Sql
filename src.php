dc9c180beaaa016de5b9aa7e47b3467259c9abdb
Paul M. Jones <pmjones88@gmail.com>
2012-02-11 14:41:43 -0600

    add files

--- src.php
+++ src.php
@@ -1,10 +1,15 @@
 <?php
-require_once __DIR__ . '/src/Aura/Sql/Column.php';
-require_once __DIR__ . '/src/Aura/Sql/ColumnFactory.php';
 require_once __DIR__ . '/src/Aura/Sql/Adapter/AbstractAdapter.php';
 require_once __DIR__ . '/src/Aura/Sql/Adapter/Mysql.php';
 require_once __DIR__ . '/src/Aura/Sql/Adapter/Pgsql.php';
 require_once __DIR__ . '/src/Aura/Sql/Adapter/Sqlite.php';
+require_once __DIR__ . '/src/Aura/Sql/Adapter/Sqlsrv.php';
 require_once __DIR__ . '/src/Aura/Sql/AdapterFactory.php';
+require_once __DIR__ . '/src/Aura/Sql/Column.php';
+require_once __DIR__ . '/src/Aura/Sql/ColumnFactory.php';
+require_once __DIR__ . '/src/Aura/Sql/ConnectionManager.php';
+require_once __DIR__ . '/src/Aura/Sql/Exception.php';
+require_once __DIR__ . '/src/Aura/Sql/Exception/NoSuchMaster.php';
+require_once __DIR__ . '/src/Aura/Sql/Exception/NoSuchSlave.php';
 require_once __DIR__ . '/src/Aura/Sql/ProfilerInterface.php';
 require_once __DIR__ . '/src/Aura/Sql/Profiler.php';
