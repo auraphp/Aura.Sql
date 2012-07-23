96008cedac4ae68b1d6935cff5e453889b2abc08
Paul M. Jones <pmjones88@gmail.com>
2012-07-23 10:28:17 -0500

    100% coverage

--- tests/Aura/Sql/ColumnTest.php
+++ tests/Aura/Sql/ColumnTest.php
@@ -31,4 +31,35 @@ class ColumnTest extends \PHPUnit_Framework_TestCase
             $this->assertSame($expect, $col->$key);
         }
     }
+    
+    public function test__set_state()
+    {
+        $info = [
+            'name' => 'cost',
+            'type' => 'numeric',
+            'size' => 10,
+            'scale' => 2,
+            'notnull' => true,
+            'default' => null,
+            'autoinc' => false,
+            'primary' => false,
+        ];
+        
+        $col = new Column(
+            $info['name'],
+            $info['type'],
+            $info['size'],
+            $info['scale'],
+            $info['notnull'],
+            $info['default'],
+            $info['autoinc'],
+            $info['primary']
+        );
+        
+        eval('$actual = ' . var_export($col, true) . ';');
+        
+        foreach ($info as $key => $expect) {
+            $this->assertSame($expect, $actual->$key);
+        }
+    }
 }
