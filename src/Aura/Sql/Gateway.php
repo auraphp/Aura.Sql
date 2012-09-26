79746e35d3f52743a19110f0bc09a0ac5973e5f9
Paul M. Jones <pmjones88@gmail.com>
2012-09-26 09:35:49 -0700

    use an adapter locator, not a specific adapter

--- src/Aura/Sql/Gateway.php
+++ src/Aura/Sql/Gateway.php
@@ -4,21 +4,21 @@ namespace Aura\Sql;
 // table data gateway
 class Gateway
 {
-    protected $sql;
+    protected $adapters;
     
     protected $mapper;
     
     public function __construct(
-        AbstractAdapter $sql,
-        AbstractMapper  $mapper
+        AdapterLocator $adapters,
+        AbstractMapper $mapper
     ) {
-        $this->sql = $sql;
-        $this->mapper = $mapper;
+        $this->adapters = $adapters;
+        $this->mapper   = $mapper;
     }
     
-    public function getSql()
+    public function getAdapters()
     {
-        return $this->sql;
+        return $this->adapters;
     }
     
     public function getMapper()
@@ -26,19 +26,10 @@ class Gateway
         return $this->mapper;
     }
     
-    public function fetch(array $identity_values)
-    {
-        $select = $this->newSelect();
-        $select->where(
-            $this->mapper->getTablePrimaryCol() . ' IN (?)',
-            $identity_values
-        );
-        return $this->sql->fetchAll($select);
-    }
-    
     public function insert($object)
     {
-        $insert = $this->sql->newInsert();
+        $sql = $this->adapters->getWrite();
+        $insert = $sql->newInsert();
         $this->mapper->modifyInsert($insert, $object);
         $this->sql->query($insert, $insert->getData());
         return $this->sql->lastInsertId();
@@ -46,46 +37,54 @@ class Gateway
     
     public function update($new_object, $old_object = null)
     {
-        $update = $this->sql->newUpdate();
+        $sql = $this->adapters->getWrite();
+        $update = $sql->newUpdate();
         $this->mapper->modifyUpdate($update, $new_object, $old_object);
         return $this->sql->query($update, $update->getData());
     }
     
     public function delete($object)
     {
-        $delete = $this->sql->newDelete();
+        $sql = $this->adapters->getWrite();
+        $delete = $sql->newDelete();
         $this->mapper->modifyDelete($delete, $object);
         return $this->sql->query($delete, $delete->getData());
     }
     
     public function fetchAll(Select $select, array $data = [])
     {
-        return $this->sql->fetchAll($select, $data);
+        $sql = $select->getSql();
+        return $sql->fetchAll($select, $data);
     }
     
     public function fetchCol(Select $select, array $data = [])
     {
-        return $this->sql->fetchCol($select, $data);
+        $sql = $select->getSql();
+        return $sql->fetchCol($select, $data);
     }
     
     public function fetchOne(Select $select, array $data = [])
     {
-        return $this->sql->fetchOne($select, $data);
+        $sql = $select->getSql();
+        return $sql->fetchOne($select, $data);
     }
     
     public function fetchPairs(Select $select, array $data = [])
     {
-        return $this->sql->fetchPairs($select, $data);
+        $sql = $select->getSql();
+        return $sql->fetchPairs($select, $data);
     }
     
     public function fetchValue(Select $select, array $data = [])
     {
-        return $this->sql->fetchValue($select, $data);
+        $sql = $select->getSql();
+        return $sql->fetchValue($select, $data);
     }
-    
+
     public function newSelect(array $cols = [])
     {
-        $select = $this->sql->newSelect();
+        $sql = $this->adapters->getRead();
+        $select = $sql->newSelect();
         $this->mapper->modifySelect($select, $cols);
         return $select;
     }
