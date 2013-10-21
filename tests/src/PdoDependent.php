d439a6049ae4a932339e65f80e2a1a755daec602
Paul M. Jones <pmjones88@gmail.com>
2013-10-21 09:33:26 -0500

    add explicit test for PDO dependency replacement

---
+++ tests/src/PdoDependent.php
@@ -0,0 +1,20 @@
+<?php
+namespace Aura\Sql;
+
+use PDO;
+
+class PdoDependent
+{
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+    
+    public function fetchAll()
+    {
+        $stm = 'SELECT * FROM pdotest';
+        $sth = $this->pdo->prepare($stm);
+        $sth->execute();
+        return $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
