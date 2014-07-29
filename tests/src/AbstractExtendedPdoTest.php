7b7dfc1cc43f8187060c1f9657c770382929d91f
Hari KT <kthari85@gmail.com>
2014-07-29 11:27:57 +0530

    Profiler moved to its own test class. Add reset profile

--- tests/src/AbstractExtendedPdoTest.php
+++ tests/src/AbstractExtendedPdoTest.php
@@ -450,57 +450,6 @@ abstract class AbstractExtendedPdoTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($rollBackResult);
     }
 
-    public function testProfiling()
-    {
-        $this->pdo->setProfiler(new Profiler);
-
-        // leave inactive
-        $this->pdo->query("SELECT 1 FROM pdotest");
-        $profiles = $this->pdo->getProfiler()->getProfiles();
-        $this->assertEquals(0, count($profiles));
-
-        // activate
-        $this->pdo->getProfiler()->setActive(true);
-
-        $this->pdo->query("SELECT 1 FROM pdotest");
-
-        $this->pdo->exec("SELECT 2 FROM pdotest");
-
-        $this->pdo->fetchAll("SELECT 3 FROM pdotest", array('zim' => 'gir'));
-
-        $profiles = $this->pdo->getProfiler()->getProfiles();
-        $this->assertEquals(3, count($profiles));
-
-        // get the profiles, remove stuff that's variable
-        $actual = $this->pdo->getProfiler()->getProfiles();
-        foreach ($actual as $key => $val) {
-            unset($actual[$key]['duration']);
-            unset($actual[$key]['trace']);
-        }
-
-        $expect = array(
-            0 => array(
-                'function' => 'query',
-                'statement' => 'SELECT 1 FROM pdotest',
-                'bind_values' => array(),
-            ),
-            1 => array(
-                'function' => 'exec',
-                'statement' => 'SELECT 2 FROM pdotest',
-                'bind_values' => array(),
-            ),
-            2 => array(
-                'function' => 'perform',
-                'statement' => 'SELECT 3 FROM pdotest',
-                'bind_values' => array(
-                    'zim' => 'gir',
-                ),
-            ),
-        );
-
-        $this->assertSame($expect, $actual);
-    }
-
     public function testPlaceholdersInPdo()
     {
         $stm = 'SELECT * FROM pdotest WHERE id > ? AND id < :max';
