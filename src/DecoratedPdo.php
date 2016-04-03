9085af91f702ff10b6aa810679b46e49d85af9e6
Paul M. Jones <pmjones88@gmail.com>
2016-04-03 10:59:08 -0500

    allow decoration of existing PDO instance

---
+++ src/DecoratedPdo.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ * This file is part of Aura for PHP.
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
+namespace Aura\Sql;
+
+use Aura\Sql\Exception;
+use PDO;
+use PDOStatement;
+use Psr\Log\NullLogger;
+
+/**
+ *
+ * Provides array quoting, a new `perform()` method, new `fetch*()` methods,
+ * and new `yield*()` methods.
+ *
+ * @package Aura.Sql
+ *
+ */
+class DecoratedPdo extends ExtendedPdo
+{
+    /**
+     *
+     * Constructor.
+     *
+     * This overrides the parent so that it can take an existing PDO instance
+     * and decorate it with the extended methods.
+     *
+     * @param PDO $pdo An existing PDO instance to decorate.
+     *
+     * @param ProfilerInterface $profiler Records query profiles to a log.
+     *
+     */
+    public function __construct(PDO $pdo, ProfilerInterface $profiler = null)
+    {
+        $this->pdo = $pdo;
+        if ($profiler === null) {
+            $profiler = new Profiler(new NullLogger());
+        }
+        $this->setProfiler($profiler);
+    }
+
+    public function connect()
+    {
+        // already connected
+    }
+
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+}
