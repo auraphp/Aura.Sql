3127ca14da821b63d87f303f2ebb6b4e5fbd02e1
Paul M. Jones <pmjones88@gmail.com>
2017-02-05 12:00:35 -0600

    ignore double-colons when preparing named placeholders
    fixes #111 for the 2.x branch

---
+++ tests/RebuilderTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Aura\Sql;
+
+class RebuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIssue111()
+    {
+        $xpdo = new ExtendedPdo('sqlite::memory:');
+        $rebuilder = new Rebuilder($xpdo);
+        $expect = "
+            SELECT id
+            FROM users
+            WHERE created_at >= ':createdDate 00:00:00'::TIMESTAMP
+            AND Foo::bar = 'something:else'
+        ";
+        list ($actual, $values) = $rebuilder($expect, []);
+        $this->assertSame($expect, $actual);
+    }
+}
