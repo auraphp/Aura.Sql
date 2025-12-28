32993865a9fc37c2259463b219a5e43dba03be6b
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-12-28 21:49:27 +0900

    Add taint annotations for prepare() and quote()
    Per code review feedback:
    - Add @psalm-taint-sink sql to prepare() for complete coverage
    - Add @psalm-taint-escape sql to quote() to mark sanitization

--- src/PdoInterface.php
+++ src/PdoInterface.php
@@ -153,6 +153,8 @@ interface PdoInterface
      * @return \PDOStatement|false
      *
      * @see http://php.net/manual/en/pdo.prepare.php
+     *
+     * @psalm-taint-sink sql $query
      */
     public function prepare(string $query, array $options = []): PDOStatement|false;
 
@@ -186,6 +188,7 @@ interface PdoInterface
      *
      * @see http://php.net/manual/en/pdo.quote.php
      *
+     * @psalm-taint-escape sql
      */
     public function quote(string|int|array|float|null $value, int $type = PDO::PARAM_STR): string|false;
 
